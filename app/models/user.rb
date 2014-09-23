cacce046d8cb4f41c3db8c0d1d231c703416568c
oholubyev <oholubyev@heliostech.hk>
2014-09-23 14:38:17 +0300

    Added user model validations and related tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
 
+  has_secure_password
+
   has_many :collections
+
+  attr_accessor :password_digest
+
+  validates :login, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,9 +2,28 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  test "create user" do
+  test 'should create user' do
     user = User.new
     assert user
+  end
+
+  test 'should create user with login and password' do
+    user = User.new(login: 'username', password: 'password')
+    assert user.valid?
+    assert user.save
+  end
+
+  test 'should not validate user with empty login or password' do
+    user = User.new(login: '', password: 'password')
+    assert !user.valid?
+
+    user = User.new(login: 'login', password: '')
+    assert !user.valid?
+  end
+
+  test 'should not authenticate user with incorrect password' do
+    user = User.new(login: 'username', password: 'correct')
+    assert !User.find_by_login('username').try(:authenticate, 'incorrect')
   end
 
 end
