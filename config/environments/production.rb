1b9764e196577c02c82c72784f653c28d94559eb
oholubyev <oholubyev@heliostech.hk>
2014-09-24 16:40:11 +0300

    Serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,8 @@ Rails.application.configure do
   # Generate digests for assets URLs.
   config.assets.digest = true
 
+  config.serve_static_assets = true
+
   # `config.assets.precompile` has moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
